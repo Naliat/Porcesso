d015ec7e46aec9fc35d3e2b8dc4842f3837caf4d
Naliat <tailansouza@alu.ufc.br>
2024-09-03 09:55:46 -0300

    Questão 5

---
+++ questão_05.cpp
@@ -0,0 +1,15 @@
+// Como identificar qual interruptor controla qual lâmpada:
+
+// Preparação:
+
+// Ligue o interruptor A e deixe-o ligado por 5 minutos.
+// Após 5 minutos, desligue o interruptor A.
+// Ligue o interruptor B.
+// Deixe o interruptor C desligado.
+// Identificação:
+
+// Vá até a sala das lâmpadas.
+// A lâmpada acesa é controlada pelo interruptor B.
+// A lâmpada apagada e quente é controlada pelo interruptor A.
+// A lâmpada apagada e fria é controlada pelo interruptor C.
+// Dessa forma, você consegue identificar qual interruptor corresponde a cada lâmpada com apenas uma visita à sala das lâmpadas.
